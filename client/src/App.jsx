386cf63d61129fd1cf62a98a20848229721163bc
Sourav Kumar <souravkumar59139@gmail.com>
2025-07-12 18:57:48 +0530

    Complete car booking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import { useLocation, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MyBookings from "./pages/MyBookings";
+import Footer from "./components/Footer";
+import CarDetails from "./pages/CarDetails";
+import Cars from "./pages/Cars";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
+      {!isOwnerPath && <Footer />}
+    </>
   );
 };
 
