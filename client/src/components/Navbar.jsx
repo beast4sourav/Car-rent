849bf450cf104390dbb2fed2cd774d0e429d7064
Sourav Kumar <souravkumar59139@gmail.com>
2025-07-13 08:38:41 +0530

    some error fix in this commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = ({ setShowLogin }) => {
       ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}
       >
         {menuLinks.map((link, index) => (
-          <Link key={index} to={link.path}>
+          <Link key={index} to={link.path} onClick={() => setOpen(false)}>
             {link.name}
           </Link>
         ))}
