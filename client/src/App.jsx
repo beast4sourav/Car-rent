82fc7ffbd4bd15496dfa89ab01ccb424e1056a85
Sourav Kumar <souravkumar59139@gmail.com>
2025-07-13 23:36:19 +0530

    complete the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,15 @@ import Dashboard from "./pages/owner/Dashboard";
 import Addcar from "./pages/owner/Addcar";
 import ManageCars from "./pages/owner/ManageCars";
 import ManageBookings from "./pages/owner/ManageBookings";
+import Login from "./components/Login";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   const isOwnerPath = useLocation().pathname.startsWith("/owner");
   return (
     <>
+    {showLogin && <Login setShowLogin={setShowLogin}/>}
+    
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
